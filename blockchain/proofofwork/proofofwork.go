5a0dbe38e811f232ea4eb2110849f08546e3c8d6
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-06-16 00:25:37 +0530

    1. Added Validate() which validates PoW in proofofwork.go 2.  Used the Validate() method in main.go

--- blockchain/proofofwork/proofofwork.go
+++ blockchain/proofofwork/proofofwork.go
@@ -83,3 +83,18 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 
 	return nonce, hash[:]
 }
+
+/*
+Validate validates PoW
+*/
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+
+	return isValid
+}
--- main.go
+++ main.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+
 	"./blockchain"
+	"./blockchain/proofofwork"
 )
 
-func main()  {
+func main() {
 	bc := blockchain.NewBlockchain()
 
 	bc.AddBlock("Send 1 BTC to Neo")
 	bc.AddBlock("Send 2 more BTC to Neo")
 
 	for _, block := range bc.Blocks {
+		pow := proofofwork.NewProofOfWork(block)
+		
 		fmt.Printf("Prev. Hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 }
