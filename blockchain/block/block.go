b7e836c3aa7931a58e7b78d61117d69646249db0
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-06-17 11:07:00 +0530

    Added Serialize(), DeserializeBlock() functions to serialize and deserialize block

--- blockchain/block/block.go
+++ blockchain/block/block.go
@@ -3,6 +3,12 @@ Package block contains datastructures, methods related to block of a blockchain
 */
 package block
 
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
+
 /*
 Block is basic datastructure of blockchain.  It represents the block of
 a blockchain
@@ -13,4 +19,38 @@ type Block struct {
 	PrevBlockHash []byte
 	Hash          []byte
 	Nonce         int
+}
+
+/*
+Serialize serializes block structure into byte array and returns slice
+of the array
+*/
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return result.Bytes()
+}
+
+/*
+DeserializeBlock deserializes a byte array to block structure and returns
+pointer to decoded block
+*/
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &block
 }
