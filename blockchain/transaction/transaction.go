9b68ab8f096a6b4cec3073ac05271ba46fba71e5
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-06-17 15:39:01 +0530

    1.  Added package `transaction` in transaction.go. 2.  Added types `Transaction`, `TxInput`, `TxOutput` in package `transaction`.

---
+++ blockchain/transaction/transaction.go
@@ -0,0 +1,26 @@
+// Package transaction contains datastructures, methods related to blockchain
+// transaction
+package transaction
+
+// Transaction represents a blockchain transaction
+type Transaction struct {
+	// ID is transaction id
+	ID []byte
+	// Vin is array of transaction inputs
+	Vin []TxInput
+	// Vout is array of transaction outputs
+	Vout []TxOutput
+}
+
+// TxInput is a transaction input
+type TxInput struct {
+	TxID      []byte
+	Vout      int
+	ScriptSig string
+}
+
+// TxOutput is a transaction output
+type TxOutput struct {
+	Value        int
+	ScriptPubKey string
+}
