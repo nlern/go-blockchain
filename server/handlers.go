698f6922466a21106a18071a35d6f5ccf7b35bf5
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-06-24 21:22:30 +0530

    Implemented handleInv.

--- server/handlers.go
+++ server/handlers.go
@@ -1,14 +1,23 @@
 package server
 
 import (
+	"bytes"
+	"encoding/hex"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
 
+	"github.com/nlern/go-blockchain/transaction"
+
 	"github.com/nlern/go-blockchain/utils"
 
 	"github.com/nlern/go-blockchain/blockchain"
+)
+
+var (
+	blocksInTransit = [][]byte{}
+	memPool         = make(map[string]transaction.Transaction)
 )
 
 func handleConnection(conn net.Conn, bc *blockchain.Blockchain) {
@@ -22,6 +31,8 @@ func handleConnection(conn net.Conn, bc *blockchain.Blockchain) {
 	switch command {
 	case "getblocks":
 		handleGetBlocks(request, bc)
+	case "inv":
+		handleInv(request, bc)
 	case "version":
 		handleVersion(request, bc)
 	default:
@@ -40,6 +51,39 @@ func handleGetBlocks(request []byte, bc *blockchain.Blockchain) {
 
 	blocks := bc.GetBlockHashes()
 	sendInv(payload.AddrFrom, "block", blocks)
+}
+
+func handleInv(request []byte, bc *blockchain.Blockchain) {
+	var payload inv
+	err := utils.Deserialize(nil, request, &payload)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Printf("Recevied inventory with %d %s\n", len(payload.Items), payload.Type)
+
+	if payload.Type == "block" {
+		blocksInTransit = payload.Items
+
+		blockHash := payload.Items[0]
+		sendGetData(payload.AddrFrom, "block", blockHash)
+
+		newInTransit := [][]byte{}
+		for _, block := range blocksInTransit {
+			if bytes.Compare(block, blockHash) != 0 {
+				newInTransit = append(newInTransit, block)
+			}
+		}
+		blocksInTransit = newInTransit
+	}
+
+	if payload.Type == "tx" {
+		txID := payload.Items[0]
+
+		if _, present := memPool[hex.EncodeToString(txID)]; present == false {
+			sendGetData(payload.AddrFrom, "tx", txID)
+		}
+	}
 }
 
 func handleVersion(request []byte, bc *blockchain.Blockchain) {
